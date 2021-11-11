c745bb811871659c158d47da1d26207966b579b1
s2236141 <p.vaish@student.utwente.nl>
2021-11-11 12:20:51 +0100

    Fixed error in Tests

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -3,12 +3,12 @@ from addition.simple import add
 
 
 def test_zero_addition():
-    assert add(0, 0) is 0
+    assert add(0, 0) == 0
 
 
 def test_identity_add():
-    assert add(0, 1) is 1
-    assert add(1, 0) is 1
+    assert add(0, 1) == 1
+    assert add(1, 0) == 1
 
 
 @pytest.mark.parametrize("test_input,expected", [
@@ -19,12 +19,12 @@ def test_identity_add():
     ((1, 4), 5)
 ])
 def test_multi_slaps(test_input, expected):
-    assert add(*test_input) is expected
+    assert add(*test_input) == expected
 
 
 @pytest.mark.skip(reason="negative numbers not supported yet")
 def test_regex_slaps():
-    assert add(-1, -1) is -2
+    assert add(-1, -1) == -2
 
 
 @pytest.mark.xfail
